6d2c8718bceb62c984f023197316351f9e0f4176
Matt Fannin <mfannin@umich.edu>
2025-04-07 21:08:39 -0400

    First commit

---
+++ hugging_face_test/test.py
@@ -0,0 +1,14 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "sd-legacy/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = pipe.to(device)
+print(pipe)
+# prompt = "a strong, but elegant man saving the day"
+# image = pipe(prompt).images[0]  
+    
+# image.save("test.png")
+
