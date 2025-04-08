d11da1457a9ee259ef21e18cce9ae6c3fc482689
mfannin <mattfannin6@gmail.com>
2025-04-07 22:03:37 -0400

    got the image generator to work

--- hugging_face_test/test.py
+++ hugging_face_test/test.py
@@ -2,17 +2,23 @@ from diffusers import StableDiffusionPipeline
 import torch
 
 model_id = "sd-legacy/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load pipeline (this was added by chatgpt and seemed to be the difference to make the script run)
+if device == "cuda":
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+else:
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+
 pipe = pipe.to(device)
+
 print(pipe)
-# prompt = "a strong, but elegant man saving the day"
-# image = pipe(prompt).images[0]  
-    
-# image.save("test.png")
+print("-------------------")
+print('Begin Generating the image')
 
-## oof
+prompt = "a strong, but elegant, nerdy, goofy man in a business meeting"
 
-## Testing the visual studio commit
-## Another change
+image = pipe(prompt).images[0]
+
+image.save("test.png")
