b600c9e27817378922d75a097569e8bcc609260e
mfannin <mattfannin6@gmail.com>
2025-10-21 19:40:18 -0400

    Working on script logic is there

--- Misc/gfl_punishment_selection.py
+++ Misc/gfl_punishment_selection.py
@@ -28,6 +28,7 @@ hot_ones_d = {
     3: "Jalapeño",
     4: "Another Hot Pepper"
 }
+weights = [30, 10, 30, 30]
 
 ## Beginning of Random Number Generator
 
@@ -36,3 +37,16 @@ random_value = random.randint(1,6)
 print("spinning wheel...")
 time.sleep(2)
 print("The Weekly Punishment in The GFL will be, ", punishments_d[random_value])
+
+if random_value == 2:
+    second_spin_value = random.randint(1,5)
+    print("spinning wheel for william dafoe pose...")
+    time.sleep(2)
+    print("The William Dafoe pose will be, ", william_dafoe_d[second_spin_value])
+elif random_value == 3:
+    second_spin_value = random.choices(list(hot_ones_d.keys()), weights=weights, k=1)[0]
+    print("spinning wheel for Hot Ones...")
+    time.sleep(2)
+    print("The hot food will be, ", hot_ones_d[second_spin_value])
+else:
+    print("Better Luck next week")
