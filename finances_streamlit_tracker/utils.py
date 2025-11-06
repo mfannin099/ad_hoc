d08e2aa1c21f3ae29e89af371f97c395ad55df2f
mfannin <mattfannin6@gmail.com>
2025-11-05 21:37:41 -0500

    Cont. Analysis on Transactions

--- finances_streamlit_tracker/utils.py
+++ finances_streamlit_tracker/utils.py
@@ -11,7 +11,14 @@ def clean_df(df):
     df['Was Venmoed'] = ' '
     df["Split Count"] = df.get("Split Count", 1)
 
-    df['Description'] = df['Description'].astype(str).str.replace(r"#\d+.*", "", regex=True).str.strip()
+    # df['Description'] = df['Description'].astype(str).str.replace(r"#\d+.*", "", regex=True).str.strip()
+    df['Description'] = (
+    df['Description']
+    .astype(str)  # ensure string type
+    .str.replace(r'[^A-Za-z\s]', '', regex=True)  # keep only letters and spaces
+    .str.replace(r'\s+', ' ', regex=True)  # collapse multiple spaces
+    .str.strip()  # remove leading/trailing spaces
+)
 
     # End Manupulation of dataset
 
